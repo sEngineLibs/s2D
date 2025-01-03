289619e6dd7c6fd031afd0f8f8181ce309f8c316
smoothie-ws <kibot.email@gmail.com>
2025-01-03 20:50:30 +1000

    Update `khafile.js`

--- khafile.js
+++ khafile.js
@@ -3,12 +3,12 @@ project.addAssets("Assets/**");
 project.addShaders("Shaders/**");
 project.addSources("Sources");
 
-// S2D Compiler flags
-project.addDefine("S2D_PP");
-// project.addDefine("S2D_PP_DOF");
-project.addDefine("S2D_PP_MIST");
-project.addDefine("S2D_PP_FISHEYE");
-project.addDefine("S2D_PP_FILTER");
-project.addDefine("S2D_PP_COMPOSITOR");
+// Available Compiler Flags:
+// S2D_PP -> enables Post-Processing
+// S2D_PP_DOF -> enables Depth of Field PP effect
+// S2D_PP_MIST -> enables Mist PP effect
+// S2D_PP_FISHEYE -> enables Fisheye PP effect
+// S2D_PP_FILTER -> enables 3x3 image convolution multi-pass PP effects
+// S2D_PP_COMPOSITOR -> enables compositor / single-pass combination of various (AA, CC etc.) PP effects
 
 resolve(project);
